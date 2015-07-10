9c2e62f20fb5c0a358931ffc8e3c48c46c3f61ee
ibdimock <ibdimock@gmail.com>
2015-07-09 21:15:25 -0400

    Conga line

--- game/dota_addons/worm_war/scripts/vscripts/tail_growth.lua
+++ game/dota_addons/worm_war/scripts/vscripts/tail_growth.lua
@@ -1,19 +1,31 @@
-
 function TailSpawn(keys)
 	local caster = keys.caster
 	local ability = keys.ability
 
-	local headPos = caster:GetAbsOrigin()
-	local dir = caster:GetForwardVector()
-
+	if caster.tailLength == nil then
+		caster.tailLength = 0
+		caster.followUnits = {caster}
+	end
+	
+	local toFollow = caster.followUnits[caster.tailLength+1]
+	local headPos = toFollow:GetAbsOrigin()
+	local dir = toFollow:GetForwardVector()
+	
 	local spawnPoint = headPos - (dir * 150)
 	--print(spawnPoint)
 	local hBug = CreateUnitByName( "npc_dota_creature_tail_bug", spawnPoint, true, caster, caster:GetOwner(), caster:GetTeamNumber() )
 		if hBug ~= nil then
+			table.insert(caster.followUnits, hBug)
+			caster.tailLength = caster.tailLength + 1
+		
 			hBug:SetForwardVector(dir)
-			hBug:SetControllableByPlayer( caster:GetPlayerID(), false )
 			hBug:SetTeam(caster:GetTeamNumber())
 			hBug:SetOwner( caster )
-			hBug:MoveToPosition(caster:GetAbsOrigin())
+			
+			ExecuteOrderFromTable({	
+				UnitIndex = hBug:GetEntityIndex(),
+				OrderType = DOTA_UNIT_ORDER_MOVE_TO_TARGET,
+				TargetIndex = caster.followUnits[caster.tailLength]:GetEntityIndex(),
+				Queue = true	})
 		end
 end
