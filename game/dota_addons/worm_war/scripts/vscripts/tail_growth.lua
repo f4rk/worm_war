3a5ae21beec20e18c3b80ba72ef0835cac141dc0
ibdimock <ibdimock@gmail.com>
2015-07-10 15:39:09 -0400

    Make pigs/gold sheep spawn 2/5

--- game/dota_addons/worm_war/scripts/vscripts/tail_growth.lua
+++ game/dota_addons/worm_war/scripts/vscripts/tail_growth.lua
@@ -1,19 +1,33 @@
 function TailSpawn(keys)
 	local caster = keys.caster
 	local ability = keys.ability
+	local unit = keys.unit
+	local unitName = unit:GetUnitName()
+	
+	local numToSpawn = 0
 
-	if caster.tailLength == nil then
-		caster.tailLength = 0
-		caster.followUnits = {caster}
+	if unitName == "npc_dota_creature_sheep" then
+		numToSpawn = 1
+	elseif unitName == "npc_dota_creature_pig" then
+		numToSpawn = 2
+	elseif unitName == "npc_dota_creature_gold_sheep" then
+		numToSpawn = 5
 	end
-	
-	local toFollow = caster.followUnits[caster.tailLength+1]
-	local headPos = toFollow:GetAbsOrigin()
-	local dir = toFollow:GetForwardVector()
-	
-	local spawnPoint = headPos - (dir * 150)
-	--print(spawnPoint)
-	local hBug = CreateUnitByName( "npc_dota_creature_tail_bug", spawnPoint, true, caster, caster:GetOwner(), caster:GetTeamNumber() )
+
+
+	for i=1,numToSpawn do
+		if caster.tailLength == nil then
+			caster.tailLength = 0
+			caster.followUnits = {caster}
+		end
+		
+		local toFollow = caster.followUnits[caster.tailLength+1]
+		local headPos = toFollow:GetAbsOrigin()
+		local dir = toFollow:GetForwardVector()
+		
+		local spawnPoint = headPos - (dir * 150)
+		--print(spawnPoint)
+		local hBug = CreateUnitByName( "npc_dota_creature_tail_bug", spawnPoint, true, caster, caster:GetOwner(), caster:GetTeamNumber() )
 		if hBug ~= nil then
 			table.insert(caster.followUnits, hBug)
 			caster.tailLength = caster.tailLength + 1
@@ -28,4 +42,5 @@ function TailSpawn(keys)
 				TargetIndex = caster.followUnits[caster.tailLength]:GetEntityIndex(),
 				Queue = true	})
 		end
+	end
 end
