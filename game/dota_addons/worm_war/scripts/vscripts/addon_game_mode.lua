faf732a8e92c9ded99c70703a20b952825d14438
unknown <andrewm.dimock@gmail.com>
2015-07-11 23:10:58 -0400

    Panorama UI, food spawn change

--- game/dota_addons/worm_war/scripts/vscripts/addon_game_mode.lua
+++ game/dota_addons/worm_war/scripts/vscripts/addon_game_mode.lua
@@ -8,8 +8,9 @@ end
 --package.path = package.path .. "../?.lua"
 --print(package.path)
 
-
+--require( "os" )
 require( "events" )
+require( "utility_functions")
 
 function Precache( context )
 	--[[
@@ -44,43 +45,324 @@ end
 
 function CWormWarGameMode:InitGameMode()
 	print( "Worm War is loaded." )
+	--math.randomseed( os.time() )
 	local GameMode = GameRules:GetGameModeEntity()
 
-	GameRules:SetPreGameTime( 10 )
-	GameMode:SetFixedRespawnTime( 5 )
-	GameMode:SetFogOfWarDisabled(true)
-	
+	--TEAM COLOURS
+	self.m_TeamColors = {}
+	self.m_TeamColors[DOTA_TEAM_GOODGUYS] = { 61, 210, 150 }	--		Teal
+	self.m_TeamColors[DOTA_TEAM_BADGUYS]  = { 243, 201, 9 }		--		Yellow
+	self.m_TeamColors[DOTA_TEAM_CUSTOM_1] = { 197, 77, 168 }	--      Pink
+	self.m_TeamColors[DOTA_TEAM_CUSTOM_2] = { 255, 108, 0 }		--		Orange
+	self.m_TeamColors[DOTA_TEAM_CUSTOM_3] = { 52, 85, 255 }		--		Blue
+	self.m_TeamColors[DOTA_TEAM_CUSTOM_4] = { 101, 212, 19 }	--		Green
+	self.m_TeamColors[DOTA_TEAM_CUSTOM_5] = { 129, 83, 54 }		--		Brown
+	self.m_TeamColors[DOTA_TEAM_CUSTOM_6] = { 27, 192, 216 }	--		Cyan
+	self.m_TeamColors[DOTA_TEAM_CUSTOM_7] = { 199, 228, 13 }	--		Olive
+	self.m_TeamColors[DOTA_TEAM_CUSTOM_8] = { 140, 42, 244 }	--		Purple
 
+	for team = 0, (DOTA_TEAM_COUNT-1) do
+		color = self.m_TeamColors[ team ]
+		if color then
+			SetTeamCustomHealthbarColor( team, color[1], color[2], color[3] )
+		end
+	end
+
+	self.m_VictoryMessages = {}
+	self.m_VictoryMessages[DOTA_TEAM_GOODGUYS] = "#VictoryMessage_GoodGuys"
+	self.m_VictoryMessages[DOTA_TEAM_BADGUYS]  = "#VictoryMessage_BadGuys"
+	self.m_VictoryMessages[DOTA_TEAM_CUSTOM_1] = "#VictoryMessage_Custom1"
+	self.m_VictoryMessages[DOTA_TEAM_CUSTOM_2] = "#VictoryMessage_Custom2"
+	self.m_VictoryMessages[DOTA_TEAM_CUSTOM_3] = "#VictoryMessage_Custom3"
+	self.m_VictoryMessages[DOTA_TEAM_CUSTOM_4] = "#VictoryMessage_Custom4"
+	self.m_VictoryMessages[DOTA_TEAM_CUSTOM_5] = "#VictoryMessage_Custom5"
+	self.m_VictoryMessages[DOTA_TEAM_CUSTOM_6] = "#VictoryMessage_Custom6"
+	self.m_VictoryMessages[DOTA_TEAM_CUSTOM_7] = "#VictoryMessage_Custom7"
+	self.m_VictoryMessages[DOTA_TEAM_CUSTOM_8] = "#VictoryMessage_Custom8"
+
+	self.m_GatheredShuffledTeams = {}
+	self.SEGMENTS_TO_WIN = 20
+	self.CLOSE_TO_VICTORY_THRESHOLD = 5
+	self.SEGMENT_PER_KILL = 5
+	self.FOOD_LIMIT = 20
+	self.NUM_CENTRE_FOOD = 5
+	self.NUM_SUPER_FOOD = 3
+
+	self.leadingTeam = -1
+	self.runnerupTeam = -1
+	self.leadingTeamScore = 0
+	self.runnerupTeamScore = 0
+	self.isGameTied = true
+	self.countdownEnabled = false
+
+	self:GatherAndRegisterValidTeams()
+
+	GameRules:GetGameModeEntity().COverthrowGameMode = self
+
+	--------------------------------------------------------------------------
+	-- Show the ending scoreboard immediately
+	GameRules:SetCustomGameEndDelay( 0 )
+	GameRules:SetCustomVictoryMessageDuration( 10 )
+	GameRules:SetPreGameTime( 10 )
+	GameMode:SetFixedRespawnTime( 1 )
+	GameMode:SetFogOfWarDisabled(true)
+	GameRules:GetGameModeEntity():SetTopBarTeamValuesOverride( true )
+	GameRules:GetGameModeEntity():SetTopBarTeamValuesVisible( false )
+	
+	ListenToGameEvent( "game_rules_state_change", Dynamic_Wrap( CWormWarGameMode, 'OnGameRulesStateChange' ), self )
 	spawnListener = ListenToGameEvent("npc_spawned", Dynamic_Wrap(CWormWarGameMode, "OnNPCSpawned"), self) 
+	ListenToGameEvent( "entity_killed", Dynamic_Wrap( CWormWarGameMode, 'OnEntityKilled' ), self )
+
 	GameMode:SetThink( "OnThink", self, "GlobalThink", 2 )
+
+	--- Spawn initial Food
+	for i = 1, self.FOOD_LIMIT do
+		CWormWarGameMode:SpawnFoodEntity(1, false )
+	end
+
+	-- Spawn inital food in centre
+	for i = 1, self.NUM_CENTRE_FOOD do
+		CWormWarGameMode:SpawnFoodEntity(1, true )
+	end
+
+	-- Spawn inital pigs
+	for i = 1, self.NUM_SUPER_FOOD do
+		CWormWarGameMode:SpawnFoodEntity(2, false )
+	end
+
 end
 
 -- Evaluate the state of the game
 function CWormWarGameMode:OnThink()
+	for nPlayerID = 0, (DOTA_MAX_TEAM_PLAYERS-1) do
+		self:UpdatePlayerColor( nPlayerID )
+	end
+
+	self:UpdateScoreboard()
+	-- Stop thinking if game is paused
+	if GameRules:IsGamePaused() == true then
+        return 1
+    end
+
+    if self.countdownEnabled == true then
+		CountdownTimer()
+		if nCOUNTDOWNTIMER == 30 then
+			CustomGameEventManager:Send_ServerToAllClients( "timer_alert", {} )
+		end
+		if nCOUNTDOWNTIMER <= 0 then
+			--Check to see if there's a tie
+			if self.isGameTied == false then
+				GameRules:SetCustomVictoryMessage( self.m_VictoryMessages[self.leadingTeam] )
+				COverthrowGameMode:EndGame( self.leadingTeam )
+				self.countdownEnabled = false
+			--[[else
+				self.TEAM_KILLS_TO_WIN = self.leadingTeamScore + 1
+				local broadcast_killcount = 
+				{
+					killcount = self.TEAM_KILLS_TO_WIN
+				}
+				CustomGameEventManager:Send_ServerToAllClients( "overtime_alert", broadcast_killcount )]]--
+			end
+       	end
+	end
+
 	if GameRules:State_Get() == DOTA_GAMERULES_STATE_GAME_IN_PROGRESS then
 		--print( "Template addon script is running." )
-		if spawnListener then
-			local innateSpells = StopListeningToGameEvent(spawnListener) 
-		end
-
-		CWormWarGameMode:DetermineAnimalSpawn()
-
+		
+		
 	elseif GameRules:State_Get() >= DOTA_GAMERULES_STATE_POST_GAME then
 		return nil
 	end
+
+
 	return 1
 end
 
-function CWormWarGameMode:OnNPCSpawned(keys)
-    local hero = EntIndexToHScript(keys.entindex)
-    if hero:IsHero() then
-    	hero:SetAbilityPoints(0)
-        local Ability1 = hero:FindAbilityByName("devour_aura")
-        local Ability2 = hero:FindAbilityByName("tail_growth")
-        if Ability1 and Ability2 then
-            print('hero Spawned leveling spells')
-            Ability1:SetLevel(1)
-            Ability2:SetLevel(1)
-        end
-    end
+
+---------------------------------------------------------------------------
+-- Get the color associated with a given teamID
+---------------------------------------------------------------------------
+function CWormWarGameMode:ColorForTeam( teamID )
+	local color = self.m_TeamColors[ teamID ]
+	if color == nil then
+		color = { 255, 255, 255 } -- default to white
+	end
+	return color
+end
+
+---------------------------------------------------------------------------
+---------------------------------------------------------------------------
+function CWormWarGameMode:EndGame( victoryTeam )
+	GameRules:SetGameWinner( victoryTeam )
+end
+
+function CWormWarGameMode:UpdatePlayerColor( nPlayerID )
+	if not PlayerResource:HasSelectedHero( nPlayerID ) then
+		return
+	end
+
+	local hero = PlayerResource:GetSelectedHeroEntity( nPlayerID )
+	if hero == nil then
+		return
+	end
+
+	local teamID = PlayerResource:GetTeam( nPlayerID )
+	local color = self:ColorForTeam( teamID )
+	PlayerResource:SetCustomPlayerColor( nPlayerID, color[1], color[2], color[3] )
+end
+
+function CWormWarGameMode:UpdateScoreboard()
+	local sortedTeams = {}
+	for _, team in pairs( self.m_GatheredShuffledTeams ) do
+		table.insert( sortedTeams, { teamID = team, teamScore = team } )
+	end
+
+	-- reverse-sort by score
+	table.sort( sortedTeams, function(a,b) return ( a.teamScore > b.teamScore ) end )
+
+	for _, t in pairs( sortedTeams ) do
+		local clr = self:ColorForTeam( t.teamID )
+
+		-- Scaleform UI Scoreboard
+		local score = 
+		{
+			team_id = t.teamID,
+			team_score = t.teamScore
+		}
+		FireGameEvent( "score_board", score )
+	end
+	-- Leader effects (moved from OnTeamKillCredit)
+	local leader = sortedTeams[1].teamID
+	--print("Leader = " .. leader)
+	self.leadingTeam = leader
+	self.runnerupTeam = sortedTeams[2].teamID
+	self.leadingTeamScore = sortedTeams[1].teamScore
+	self.runnerupTeamScore = sortedTeams[2].teamScore
+	if sortedTeams[1].teamScore == sortedTeams[2].teamScore then
+		self.isGameTied = true
+	else
+		self.isGameTied = false
+	end
+	local allHeroes = HeroList:GetAllHeroes()
+	for _,entity in pairs( allHeroes) do
+		if entity:GetTeamNumber() == leader and sortedTeams[1].teamScore ~= sortedTeams[2].teamScore then
+			if entity:IsAlive() == true then
+				-- Attaching a particle to the leading team heroes
+				local existingParticle = entity:Attribute_GetIntValue( "particleID", -1 )
+       			if existingParticle == -1 then
+       				local particleLeader = ParticleManager:CreateParticle( "particles/leader/leader_overhead.vpcf", PATTACH_OVERHEAD_FOLLOW, entity )
+					ParticleManager:SetParticleControlEnt( particleLeader, PATTACH_OVERHEAD_FOLLOW, entity, PATTACH_OVERHEAD_FOLLOW, "follow_overhead", entity:GetAbsOrigin(), true )
+					entity:Attribute_SetIntValue( "particleID", particleLeader )
+				end
+			else
+				local particleLeader = entity:Attribute_GetIntValue( "particleID", -1 )
+				if particleLeader ~= -1 then
+					ParticleManager:DestroyParticle( particleLeader, true )
+					entity:DeleteAttribute( "particleID" )
+				end
+			end
+		else
+			local particleLeader = entity:Attribute_GetIntValue( "particleID", -1 )
+			if particleLeader ~= -1 then
+				ParticleManager:DestroyParticle( particleLeader, true )
+				entity:DeleteAttribute( "particleID" )
+			end
+		end
+	end
+end
+
+---------------------------------------------------------------------------
+-- Scan the map to see which teams have spawn points
+---------------------------------------------------------------------------
+function CWormWarGameMode:GatherAndRegisterValidTeams()
+--	print( "GatherValidTeams:" )
+
+	local foundTeams = {}
+	for _, playerStart in pairs( Entities:FindAllByClassname( "info_player_start_dota" ) ) do
+		foundTeams[  playerStart:GetTeam() ] = true
+	end
+
+	local numTeams = TableCount(foundTeams)
+	print( "GatherValidTeams - Found spawns for a total of " .. numTeams .. " teams" )
+	
+	local foundTeamsList = {}
+	for t, _ in pairs( foundTeams ) do
+		table.insert( foundTeamsList, t )
+	end
+
+	if numTeams == 0 then
+		print( "GatherValidTeams - NO team spawns detected, defaulting to GOOD/BAD" )
+		table.insert( foundTeamsList, DOTA_TEAM_GOODGUYS )
+		table.insert( foundTeamsList, DOTA_TEAM_BADGUYS )
+		numTeams = 2
+	end
+
+	local maxPlayersPerValidTeam = math.floor( 10 / numTeams )
+
+	self.m_GatheredShuffledTeams = ShuffledList( foundTeamsList )
+
+	print( "Final shuffled team list:" )
+	for _, team in pairs( self.m_GatheredShuffledTeams ) do
+		print( " - " .. team .. " ( " .. GetTeamName( team ) .. " )" )
+	end
+
+	print( "Setting up teams:" )
+	for team = 0, (DOTA_TEAM_COUNT-1) do
+		local maxPlayers = 0
+		if ( nil ~= TableFindKey( foundTeamsList, team ) ) then
+			maxPlayers = maxPlayersPerValidTeam
+		end
+		print( " - " .. team .. " ( " .. GetTeamName( team ) .. " ) -> max players = " .. tostring(maxPlayers) )
+		GameRules:SetCustomGameTeamMaxPlayers( team, maxPlayers )
+	end
+end
+---------------------------------------------------------------------------
+-- Event: Determine animal to spawn
+---------------------------------------------------------------------------
+function CWormWarGameMode:DetermineAnimalSpawn()
+	local r = RandomInt( 1, 100 )
+	if r > 25 then
+		CWormWarGameMode:SpawnFoodLocation(1) -- Regular Sheep
+	elseif r > 5 and r <=25 then
+		CWormWarGameMode:SpawnFoodLocation(2) -- Pig
+	else
+		CWormWarGameMode:SpawnFoodLocation(3) -- Golden Sheep
+	end
+end
+
+---------------------------------------------------------------------------
+-- Event: Spawn Sheep
+---------------------------------------------------------------------------
+function CWormWarGameMode:SpawnFoodLocation(foodType, centre)
+	local r1 = RandomInt( -40, 40 )
+	local r2 = RandomInt( -40, 40 )
+	local xpos = 0
+	local ypos = 0
+	
+	if centre then
+		xpos = r1*100; -- Coordinates within centre of arena
+		ypos = r2*100;
+	else
+		xpos = r1*200; -- Coordinates within arena
+		ypos = r2*200;
+	end
+
+	local spawnPoint = Vector(xpos, ypos, 0)
+	print("sheep position: " .. xpos..", ".. ypos)
+	return spawnPoint
+end
+
+function CWormWarGameMode:SpawnFoodEntity(foodType, centre)
+	local spawnPoint = CWormWarGameMode:SpawnFoodLocation(foodType, centre)
+	--print( spawnPoint )
+	if foodType == 1 then
+		local sheep = CreateUnitByName( "npc_dota_creature_sheep", spawnPoint, true, nil, nil, DOTA_TEAM_NEUTRALS )
+	elseif foodType == 2 then
+		--EmitGlobalSound("General.Pig")
+		local sheep = CreateUnitByName( "npc_dota_creature_pig", spawnPoint, true, nil, nil, DOTA_TEAM_NEUTRALS )
+	elseif foodType == 3 then
+		--EmitGlobalSound("Item.PickUpGemWorld")
+		local sheep = CreateUnitByName( "npc_dota_creature_gold_sheep", spawnPoint, true, nil, nil, DOTA_TEAM_NEUTRALS )
+	end
+	
 end
--- game/dota_addons/worm_war/scripts/vscripts/events.lua
+++ game/dota_addons/worm_war/scripts/vscripts/events.lua
@@ -1,40 +1,151 @@
 --[[ events.lua ]]
 ---------------------------------------------------------------------------
--- Event: Determine animal to spawn
+-- Event: Game state change handler
 ---------------------------------------------------------------------------
-function CWormWarGameMode:DetermineAnimalSpawn()
-	local r = RandomInt( 1, 100 )
-	if r > 25 then
-		CWormWarGameMode:SpawnFoodLocation(1) -- Regular Sheep
-	elseif r > 5 and r <=25 then
-		CWormWarGameMode:SpawnFoodLocation(2) -- Pig
-	else
-		CWormWarGameMode:SpawnFoodLocation(3) -- Golden Sheep
+function CWormWarGameMode:OnGameRulesStateChange()
+	local nNewState = GameRules:State_Get()
+	--print( "OnGameRulesStateChange: " .. nNewState )
+
+	if nNewState == DOTA_GAMERULES_STATE_HERO_SELECTION then
+
+	end
+
+	if nNewState == DOTA_GAMERULES_STATE_PRE_GAME then
+		local numberOfPlayers = PlayerResource:GetPlayerCount()
+		nCOUNTDOWNTIMER = 601
+		--[[if numberOfPlayers > 7 then
+			--self.TEAM_KILLS_TO_WIN = 25
+			nCOUNTDOWNTIMER = 901
+		elseif numberOfPlayers > 4 and numberOfPlayers <= 7 then
+			--self.TEAM_KILLS_TO_WIN = 20
+			nCOUNTDOWNTIMER = 721
+		else
+			--self.TEAM_KILLS_TO_WIN = 15
+			nCOUNTDOWNTIMER = 601
+		end
+		if GetMapName() == "forest_solo" then
+			self.TEAM_KILLS_TO_WIN = 20
+		elseif GetMapName() == "desert_duo" then
+			self.TEAM_KILLS_TO_WIN = 25
+		else
+			self.TEAM_KILLS_TO_WIN = 30
+		end]]--
+		--print( "Kills to win = " .. tostring(self.TEAM_KILLS_TO_WIN) )
+
+		CustomNetTables:SetTableValue( "game_state", "victory_condition", { kills_to_win = self.SEGMENT_TO_WIN } );
+
+		self._fPreGameStartTime = GameRules:GetGameTime()
+	end
+
+	if nNewState == DOTA_GAMERULES_STATE_GAME_IN_PROGRESS then
+		--print( "OnGameRulesStateChange: Game In Progress" )
+		if self.spawnListener then
+			local innateSpells = StopListeningToGameEvent(self.spawnListener) 
+		end
+
+		self.countdownEnabled = true
+		CustomGameEventManager:Send_ServerToAllClients( "show_timer", {} )
+		--DoEntFire( "center_experience_ring_particles", "Start", "0", 0, self, self  )
 	end
 end
 
----------------------------------------------------------------------------
--- Event: Spawn Sheep
----------------------------------------------------------------------------
-function CWormWarGameMode:SpawnFoodLocation(foodType)
-	local r1 = RandomInt( 0, 100 )
-	local r2 = RandomInt( 0, 100 )
-	local xpos = (r1-50)*10; --convert random coords to map coords
-	local ypos = (r2-50)*10;
-	--print("sheep position: " .. xpos..", ".. ypos)
-	CWormWarGameMode:SpawnFoodEntity(foodType, Vector( xpos, ypos, 0 ) )
+function CWormWarGameMode:OnNPCSpawned(keys)
+    local hero = EntIndexToHScript(keys.entindex)
+    if hero:IsHero() then
+    	hero:SetAbilityPoints(0)
+        local Ability1 = hero:FindAbilityByName("devour_aura")
+        local Ability2 = hero:FindAbilityByName("tail_growth")
+        if Ability1 and Ability2 then
+            print('hero Spawned leveling spells')
+            Ability1:SetLevel(1)
+            Ability2:SetLevel(1)
+        end
+    end
 end
 
-function CWormWarGameMode:SpawnFoodEntity(foodType, spawnPoint)
-	if foodType == 1 then
-		EmitGlobalSound("Hero_ShadowShaman.SheepHex.Target")
-		local sheep = CreateUnitByName( "npc_dota_creature_sheep", spawnPoint, true, nil, nil, DOTA_TEAM_NEUTRALS )
-	elseif foodType == 2 then
-		EmitGlobalSound("General.Pig")
-		local sheep = CreateUnitByName( "npc_dota_creature_pig", spawnPoint, true, nil, nil, DOTA_TEAM_NEUTRALS )
-	elseif foodType == 3 then
-		EmitGlobalSound("Item.PickUpGemWorld")
-		local sheep = CreateUnitByName( "npc_dota_creature_gold_sheep", spawnPoint, true, nil, nil, DOTA_TEAM_NEUTRALS )
-	end
+---------------------------------------------------------------------------
+-- Event: OnTeamKillCredit, see if anyone won
+---------------------------------------------------------------------------
+function CWormWarGameMode:OnTeamKillCredit( event )
+--	print( "OnKillCredit" )
+--	DeepPrint( event )
+
+	local nKillerID = event.killer_userid
+	local nTeamID = event.teamnumber
+	--local nTeamKills = event.herokills
+	--local nKillsRemaining = self.TEAM_KILLS_TO_WIN - nTeamKills
 	
+	local broadcast_kill_event =
+	{
+		killer_id = event.killer_userid,
+		team_id = event.teamnumber,
+		--team_kills = nTeamKills,
+		--kills_remaining = nKillsRemaining,
+		victory = 0,
+		close_to_victory = 0,
+		very_close_to_victory = 0,
+	}
+
+	--[[if nKillsRemaining <= 0 then
+		GameRules:SetCustomVictoryMessage( self.m_VictoryMessages[nTeamID] )
+		GameRules:SetGameWinner( nTeamID )
+		broadcast_kill_event.victory = 1
+	elseif nKillsRemaining == 1 then
+		EmitGlobalSound( "ui.npe_objective_complete" )
+		broadcast_kill_event.very_close_to_victory = 1
+	elseif nKillsRemaining <= self.CLOSE_TO_VICTORY_THRESHOLD then
+		EmitGlobalSound( "ui.npe_objective_given" )
+		broadcast_kill_event.close_to_victory = 1
+	end]]--
+
+	CustomGameEventManager:Send_ServerToAllClients( "kill_event", broadcast_kill_event )
+end
+
+---------------------------------------------------------------------------
+-- Event: OnEntityKilled
+---------------------------------------------------------------------------
+function CWormWarGameMode:OnEntityKilled( event )
+	local killedUnit = EntIndexToHScript( event.entindex_killed )
+	local killedTeam = killedUnit:GetTeam()
+	local hero = EntIndexToHScript( event.entindex_attacker )
+	local heroTeam = hero:GetTeam()
+	if killedUnit:IsRealHero() then
+		self.allSpawned = true
+		--print("Hero has been killed")
+		if hero:IsRealHero() and heroTeam ~= killedTeam then
+			--print("Granting killer xp")
+			if killedUnit:GetTeam() == self.leadingTeam and self.isGameTied == false then
+				local memberID = hero:GetPlayerID()
+				PlayerResource:ModifyGold( memberID, 500, true, 0 )
+				hero:AddExperience( 100, 0, false, false )
+				local name = hero:GetClassname()
+				local victim = killedUnit:GetClassname()
+				local kill_alert =
+					{
+						hero_id = hero:GetClassname()
+					}
+				CustomGameEventManager:Send_ServerToAllClients( "kill_alert", kill_alert )
+			else
+				hero:AddExperience( 50, 0, false, false )
+			end
+		end
+	else
+		-- HANDLE LENGTH addition + win condition
+		local nSegments = hero.tailLength
+		local nSegmentsRemaining = self.SEGMENTS_TO_WIN - nSegments
+		print("Current Segments: ".. nSegments)
+		print("Segments Remaining: ".. nSegmentsRemaining)
+		if nSegmentsRemaining <= 0 then
+			GameRules:SetCustomVictoryMessage( self.m_VictoryMessages[nTeamID] )
+			GameRules:SetGameWinner( nTeamID )
+			--broadcast_kill_event.victory = 1
+		elseif nSegmentsRemaining == 1 then
+			EmitGlobalSound( "ui.npe_objective_complete" )
+			--broadcast_kill_event.very_close_to_victory = 1
+		elseif nSegmentsRemaining <= self.CLOSE_TO_VICTORY_THRESHOLD then
+			EmitGlobalSound( "ui.npe_objective_given" )
+			--broadcast_kill_event.close_to_victory = 1
+		end
+
+	end
 end
